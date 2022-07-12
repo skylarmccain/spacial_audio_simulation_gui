ae03757ac5c881600e0078ac79d22e6ad4bc4865
skylarmccain <skylarmccain@ufl.edu>
2022-07-12 12:33:50 -0400

    more class stuff

--- Object.py
+++ Object.py
@@ -1,10 +1,34 @@
 import tkinter as tk
 
 class Object():
-    binded_audios = []
+
+   
 
     # class members
-    def __init__(self, object_name):
+    def __init__(self, object_name, x_pos_start, y_pos_start):
+        
+        
+        # class members
         self.object_name = object_name
+        self.binded_audios = [] #maybe make a dictionary for audio_name and audio_object
+        pos = tuple()
+        self.pos = (x_pos_start, y_pos_start)
+        self.start_pos = tuple(x_pos_start, y_pos_start)
+    
+    def bind_to(self, filename):
+        self.binded_audios.append(filename)
+    
+    # returns current (x,y) coordinates as tuple
+    def get_position(self):
+        return self.pos
+    
+    # returns initialized (x,y) coordinates as tuple
+    def get_start_pos(self):
+        return self.start_pos
+    
+    # set current position to new location
+    def move(self,x,y):
+        pos = (x,y)
+    
     
     
